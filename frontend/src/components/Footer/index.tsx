32c23e0923933cbbfe5b7ca5739fb6297c6d8064
Caio-Elcio <caio.eloy@bandtec.com.br>
2022-01-13 19:02:13 -0300

    Update project II

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -7,18 +7,11 @@ function Footer() {
                 <div className="container-footer">
                     <div className="sec dsmovie">
                         <h5>DSMovie</h5>
-                        <ul className="sci">
-                            <li><a href="#"><i className="fa fa-whatsapp" aria-hidden="true"></i></a></li>
-                            <li><a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a></li>
-                            <li><a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
-                            <li><a href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
-                            <li><a href="https://www.linkedin.com/in/caio-elcio-733426204" target="_blank">
-                                <i className="fa fa-linkedin" aria-hidden="true"></i></a>
-                            </li>
-                            <li><a href="https://github.com/Caio-Elcio" target="_blank">
-                                <i className="fa fa-github" aria-hidden="true"></i></a>
-                            </li>
-                        </ul>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                            Ad numquam minima repellendus ullam, praesentium unde fuga
+                            sequi accusantium ex voluptas illum similique exercitationem expedita illo
+                            officiis perspiciatis quod sit sunt.
+                        </p>
                     </div>
                     <div className="sec localizacao">
                         <h5>Localização</h5>
@@ -30,19 +23,13 @@ function Footer() {
                         <h5>Contato</h5>
                         <ul className="info">
                             <li>
-                                <p><i className="fa fa-phone" aria-hidden="true"></i>
-                                    <a href="tel:5511941282899"> +55 (11) 94022-6090</a>
-                                </p>
+                                <p><i className="fa fa-phone" aria-hidden="true"> +55 (11) 94022-6090</i></p>
                             </li>
                             <li>
-                                <p><i className="fa fa-phone" aria-hidden="true"></i>
-                                    <a href="tel:5511953930508"> +55 (11) 94028-9060</a>
-                                </p>
+                                <p><i className="fa fa-phone" aria-hidden="true"> +55 (11) 94028-9060</i></p>
                             </li>
                             <li>
-                                <p><i className="fa fa-envelope" aria-hidden="true"></i>
-                                    <a href="mailto:dsmovie@gmail.com"> dsmovie@gmail.com</a>
-                                </p>
+                                <p><i className="fa fa-envelope" aria-hidden="true"> dsmovie@gmail.com</i></p>
                             </li>
                         </ul>
                     </div>
